cb729d463beb3dcaee3feb9aa047d25aadce1407
Christine Cheung <piroko@gmail.com>
2012-04-16 21:49:00 -0700

    social media icons for nav

--- pyladies/static/js/pyladies.js
+++ pyladies/static/js/pyladies.js
@@ -1,5 +1,18 @@
-var AppNamespace = $(function () {
+var PyLadies = $(function () {
     'use strict';
-    // Change name of the Namespace variable and start writing your JavaScript here
 
-})();
+    var user = "pyladies";
+    var url = "http://twitter.com/statuses/user_timeline.json?screen_name=" + user + "&count=1&callback=?";
+
+    $.getJSON(url, function(data) {
+
+        var tweet = data[0].text;
+
+        // regex for url and twitter handle links
+        tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[\-A\-Z0\-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig, '<a href="$1">$1</a>');
+        tweet = tweet.replace(/(^|\s)@(\w+)/g, '$1<a href="http://www.twitter.com/$2">@$2</a>');
+      
+        $(".feed").html(tweet);
+    });
+
+}());
